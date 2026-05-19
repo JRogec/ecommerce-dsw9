6bbe3c6bd86a0836e148986073981df82b7f23ea
JRogec <jose.castillo41@utp.ac.pa>
2026-05-19 06:06:47 +0000

    feat: vistas EJS, estilos y JS del cliente

--- config/database.js
+++ config/database.js
@@ -24,7 +24,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host:           process.env.DB_HOST,
-    port:           parseInt(process.env.DB_PORT) || 3306,
+    port:           parseInt(process.env.DB_PORT) || 3300,
     dialect:        'mysql',
     logging:        false,
     dialectOptions: getSslConfig()
